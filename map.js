4a24d3c71d189d3a9e9826c5ae9235ea11626977
TeresaLee <teresalee@TeresaLees-MacBook-Pro-2.local>
2026-08-17 01:03:20 +0800

    Initial commit for Fukuoka trip app

---
+++ map.js
@@ -0,0 +1,25 @@
+// maps.js
+const MAP_LINKS = {
+  // Day 1: 9/15 (二)
+  "桃機T1": "https://maps.app.goo.gl/jt4WKa5dNRttwyLq5",
+  "福岡機場": "https://maps.app.goo.gl/daSVT2yj8bkurwkK6",
+  "飯店": "https://maps.app.goo.gl/vc8N2cQFvNL9E4ux9",
+  "QuilFaitBon": "https://maps.app.goo.gl/2rPaizVi4QcrDBSd6",
+  "天神地下街": "https://maps.app.goo.gl/WvSBesVSR5nUQAteA",
+  "水炊橙": "https://maps.app.goo.gl/u2dKpCboBGPYDRWs8",
+
+  // Day 2: 9/16 (三)
+  "太宰府": "https://maps.app.goo.gl/YsTgKUKVHpGnkpM5A",
+  "味之明太子": "https://maps.app.goo.gl/Wv62a4Cx6HuhF1Jb8",
+  "九州國立博物館": "https://maps.app.goo.gl/xuW7VgmVDz9jde1B6",
+  "博多運河城": "https://maps.app.goo.gl/txDXGStuzSWrStNYA",
+  "燒肉Sudo": "https://maps.app.goo.gl/rvuHrYUiYwvMsE3KA",
+
+  // Day 3: 9/17 (四)
+  "藍瓶咖啡": "https://maps.app.goo.gl/4CiAjuajvQd5C7Y57",
+  "博多集合點": "https://maps.app.goo.gl/RcLYziABPCHWT4GVA",
+  "門司港": "https://maps.app.goo.gl/RrwwAx5VoFUmnoL67",
+  "小倉城": "https://maps.app.goo.gl/dwhtreojDYZzkEPn7",
+  "北九州Outlet": "https://maps.app.goo.gl/4jLByJxqrkE5VmLMA",
+  "皿倉山夜景": "https://maps.app.goo.gl/yq4b5fFym5vxSXmV8"
+};
